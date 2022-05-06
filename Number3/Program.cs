e1981541189dd079dcbac29814378a9ddc8e4636
Gulnara <gulnara@mail.rum>
2022-05-06 21:51:19 +0600

    Number3

---
+++ Number3/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 3: Задайте массив вещественных чисел. 
+// Найдите разницу между максимальным и минимальным элементов массива.
+int[] array = {8, 10, 51, 13, 81, 56, 5};
+int maxValue = array[0];
+int minValue = array[0];
+
+for (int i = 0; i < array.Length; i++) // задаю массив вещественных чисел
+{
+    Console.Write( array[i] + " "); // вывожу их на экран
+
+    if (array[i] > maxValue) // ищем максимальный элемент массива
+    {
+        maxValue = array[i];
+    }
+
+    if (array[i] < minValue) // ищем минимальный элемент массива
+    {
+        minValue = array[i];
+    }
+}
+Console.WriteLine();
+Console.WriteLine($"Разница между максимальным элементом и минимальным элементом равна:  ");
+Console.Write(maxValue - minValue); // вывожу разницу 
+Console.WriteLine();
