a4b07e79793403948959212644b9067754d6ebd7
Gulnara <gulnara@mail.rum>
2022-05-06 21:53:20 +0600

    Number6

---
+++ Number6/Program.cs
@@ -0,0 +1,16 @@
+﻿// Выяснить являются ли три числа сторонами треугольника
+
+Console.WriteLine($"Введите три числа: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+int c = Convert.ToInt32(Console.ReadLine());
+
+if ((a + b > c) && (a + c > b) && (b + c > a)) 
+{ 
+    Console.WriteLine("Числа являются сторонами треугольника");
+}
+else Console.WriteLine("Числа не являются сторонами треугольника");
+Console.WriteLine();
+        
+
+
