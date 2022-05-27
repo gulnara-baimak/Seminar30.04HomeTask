74b969cfe393fffa1c46f923f620914bf42e39c8
Gulnara <gulnara@mail.rum>
2022-05-28 00:45:57 +0600

    Massive8Elements

---
+++ Massiv8Elements/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задать массив из 8 элементов и вывести их на экран
+// ввод с клавиатуры
+
+void FillArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        Console.Write(array[i] + " ");
+}
+int[] array = { 1, 2, 3, 4, 5, 6, 7, 8 };
+FillArray(array);
+Console.WriteLine();
