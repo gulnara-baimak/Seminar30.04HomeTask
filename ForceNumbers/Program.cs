6f27cf064f1de5331f701c70099a920835c73b94
Gulnara <gulnara@mail.rum>
2022-05-28 00:44:25 +0600

    ForceNumbers

---
+++ ForceNumbers/Program.cs
@@ -0,0 +1,14 @@
+﻿//перебор числа
+// программа, которая возвращает все целочисленные значения от 0 до текущего числа. 
+
+foreach(var n in 5) Console.WriteLine(n);
+foreach (var n in -5) Console.WriteLine(n);
+ 
+static class Int32Extension
+{
+    public static IEnumerator<int> GetEnumerator(this int number)
+    {
+        int k = (number > 0)? number: 0;
+        for (int i = number - k; i <= k; i++) yield return i;
+    }
+}
