dbd8d3a776915ab4190419261f57bb3ea07d8fdc
Gulnara <gulnara@mail.rum>
2022-05-28 00:58:48 +0600

    Matrix

---
+++ Matrix/Program.cs
@@ -0,0 +1,33 @@
+﻿// Программа, которая заполняет матрицу случайными числами
+// i - строки считает
+// j - столбцы считает
+
+void PrintArray(int[,] matrix)//  метод: выводим матрицу в консоли
+{
+    for (int i = 0; i < matrix.GetLength(0); i++) // строки
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++) // столбцы
+        {
+        Console.Write($"{matrix[i, j]} "); // косноль: выводим на экран
+        }
+        Console.WriteLine();
+    }
+}
+
+void FillArray(int[,] matr) // метод: заполняем матрицу случайными числами
+{
+    for (int i = 0; i < matr.GetLength(0); i++) // cтроки
+    {
+        for (int j = 0; j < matr.GetLength(1); j++) // столбцы
+        {
+            matr[i, j] = new Random().Next(1,10); // генератор случайных чисел
+        }
+        
+    }
+}
+int[,] matrix = new int[3,4];
+PrintArray(matrix);
+Console.WriteLine(); 
+FillArray(matrix);
+PrintArray(matrix);
+Console.WriteLine(); 
