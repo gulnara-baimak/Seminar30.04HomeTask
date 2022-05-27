5579239b32a8b3645f4b3b848cf4ebaa747706c2
Gulnara <gulnara@mail.rum>
2022-05-28 00:51:49 +0600

    Number8

---
+++ Number8/Program.cs
@@ -0,0 +1,17 @@
+﻿// Определить сколько чисел больше 0 введено с клавиатуры
+
+Console.Write("Введите числа через пробел с клавиатуры: ");
+
+int[] array = Console.ReadLine().Split(' ').Select(x => int.Parse(x)).ToArray();
+
+int Numbers(int[] array)
+{
+    int count = 0;
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0) count++;
+    }
+    return count;
+}
+Console.WriteLine(Numbers(array));
